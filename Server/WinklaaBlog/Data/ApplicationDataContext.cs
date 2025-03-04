0dcece86a6bdc80ecf15a05d22f73264dd1ab119
Winklaa22 <49796412+Winklaa22@users.noreply.github.com>
2025-03-04 14:50:41 +0100

    ApplicationDataContext

---
+++ Server/WinklaaBlog/Data/ApplicationDataContext.cs
@@ -0,0 +1,56 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace WinklaaBlog.Data
+{
+    public class ApplicationDataContext
+    {
+        private readonly IConfiguration _config;
+        public ApplicationDataContext(IConfiguration config)
+        {
+            _config = config;
+        }
+        public IDbConnection DBConnection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public IEnumerable<T> LoadData<T>(string sql)
+        {
+            return DBConnection.Query<T>(sql);
+        }
+
+        public T LoadDataSingle<T>(string sql)
+        {
+            return DBConnection.QuerySingle<T>(sql);
+        }
+
+        public bool ExecuteSql(string sql)
+        {
+            return DBConnection.Execute(sql) > 0;
+        }
+
+        public int ExecuteSqlWithRowCount(string sql)
+        {
+            return DBConnection.Execute(sql);
+        }
+
+        public bool ExecuteSqlWithParameters(string sql, IEnumerable<SqlParameter> parameters)
+        {
+            using (var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection")))
+            {
+                using var command = new SqlCommand(sql, connection);
+                command.Parameters.AddRange(parameters.ToArray());
+
+                connection.Open();
+
+                var affectedRows = command.ExecuteNonQuery();
+                return affectedRows > 0;
+            }
+        }
+    }
+}
